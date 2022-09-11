196c43b5640ec58d8c4db97a7c92a668e4099e7f
Payal Joshi <payaljoshi795@gmail.com>
2022-09-11 20:59:21 +0530

    Order module

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant.Controllers
+{
+    public class HomeController : Controller
+    {
+        RestaurantProjectEntities _context = new RestaurantProjectEntities();
+        public ActionResult Index()
+        {
+            var listofdata = _context.Orders.ToList();
+            return View(listofdata);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Orders model)
+        {
+            _context.Orders.Add(model);
+            _context.SaveChanges();
+            ViewBag.Message = "Data Inserted Successfully";
+            return View();
+        }
+
+       
+    }
+}
